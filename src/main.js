a589dabcd472c2b0abec9094b24088f1f51d9010
Stradalec <pochtatipo6@gmail.com>
2025-10-02 20:43:01 +0700

    feature: add spawn task by click to button "add"

--- src/main.js
+++ src/main.js
@@ -1,7 +1,31 @@
 const button = document.getElementById("add");
+const editButton = document.getElementsByClassName("button_task_edit");
 const main = document.querySelector("main.main")
 
-button.addEventListener('click', () =>{
+button.addEventListener('click', () => {
+    const taskSection = document.createElement("section");
+    taskSection.className = "task_window";
+    main.appendChild(taskSection);
+    let addButton = document.createElement('button');
+    addButton.type = "button";
+    addButton.className = "task_window_button";
+    let addElement = document.createElement('h2');
+    addElement.textContent = "Task Title";
+    addButton.appendChild(addElement);
+    addElement = document.createElement('p');
+    addElement.textContent = "Task description";
+    addButton.appendChild(addElement);
+    taskSection.appendChild(addButton);
+    addButton = document.createElement('button');
+    addButton.className = "button_task";
+    addElement = document.createElement("img");
+    addElement.src = "assets/vector/delete.svg";
+    addElement.alt = "Удалить заметку";
+    addButton.appendChild(addElement);
+    taskSection.appendChild(addButton);
+});
+
+editButton.addEventListener('click', () =>{
     const editSection = document.createElement("section");
     editSection.className = "edit_window";
     main.appendChild(editSection)
